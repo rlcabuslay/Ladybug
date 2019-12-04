23676fb3f06eee92a14fd08469cbfad3b1d4be5d
Hyperhawk69 <58247329+Hyperhawk69@users.noreply.github.com>
2019-12-04 14:41:44 +0800

    removed checkBounds()

--- src/ladybug/Ladybug.java
+++ src/ladybug/Ladybug.java
@@ -53,25 +53,4 @@ public class Ladybug extends Sprite{
 		if(tempY + 8 > 28 + 64 && tempY < GameStage.WINDOW_HEIGHT - (28+64+64+60))
 			this.y = tempY;    
 	}
-	
-	//method to correct the position if LADYBUG is going off-screen
-	public void checkBounds() {
-    	if(this.getX()==5||this.getX()==4||this.getX()==3||this.getX()==2||this.getX()==1||this.getX()==0||this.getX()==-1||this.getX()==-2||this.getX()==-3||this.getX()==-4) {
-    		this.setDX(0);
-    		this.setX(getX()+1);
-    	}
-    	if(this.getX()==GameStage.WINDOW_WIDTH-45||this.getX()==GameStage.WINDOW_WIDTH-44||this.getX()==GameStage.WINDOW_WIDTH-43||this.getX()==GameStage.WINDOW_WIDTH-42||this.getX()==GameStage.WINDOW_WIDTH-41||this.getX()==GameStage.WINDOW_WIDTH-40||this.getX()==GameStage.WINDOW_WIDTH-39||this.getX()==GameStage.WINDOW_WIDTH-38||this.getX()==GameStage.WINDOW_WIDTH-37||this.getX()==GameStage.WINDOW_WIDTH-36){
-    		this.setDX(0);
-    		this.setX(getX()-1);
-    	}
-    	if(this.getY()==5||this.getY()==4||this.getY()==3||this.getY()==2||this.getY()==1||this.getY()==0||this.getY()==-1||this.getY()==-2||this.getY()==-3||this.getY()==-4){
-    		this.setDY(0);
-    		this.setY(getY()+1);
-    	}
-    	if(this.getY()==GameStage.WINDOW_HEIGHT-45||this.getY()==GameStage.WINDOW_HEIGHT-44||this.getY()==GameStage.WINDOW_HEIGHT-43||this.getY()==GameStage.WINDOW_HEIGHT-42||this.getY()==GameStage.WINDOW_HEIGHT-41||this.getY()==GameStage.WINDOW_HEIGHT-40||this.getY()==GameStage.WINDOW_HEIGHT-39||this.getY()==GameStage.WINDOW_HEIGHT-38||this.getY()==GameStage.WINDOW_HEIGHT-37||this.getY()==GameStage.WINDOW_HEIGHT-36) {
-    		this.setDY(0);
-    		this.setY(getY()-1);
-    	}
-	}
-
 }

0d6b64677d147c76f9b84cdb38acc66d09ff9b3f
Hyperhawk69 <58247329+Hyperhawk69@users.noreply.github.com>
2019-12-06 22:39:34 +0800

    added animation images

--- src/ladybug/Ladybug.java
+++ src/ladybug/Ladybug.java
@@ -8,6 +8,20 @@ public class Ladybug extends Sprite{
 	private boolean alive;
 	
 	public final static Image LADYBUG_IMAGE = new Image("images/Sprites/Characters/LadyBug/LadyBugR.gif",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE2 = new Image("images/Sprites/Characters/LadyBug/LadyBug7.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE3 = new Image("images/Sprites/Characters/LadyBug/LadyBug8.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE4 = new Image("images/Sprites/Characters/LadyBug/LadyBug9.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE5 = new Image("images/Sprites/Characters/LadyBug/LadyBug10.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE6 = new Image("images/Sprites/Characters/LadyBug/LadyBug11.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE7 = new Image("images/Sprites/Characters/LadyBug/LadyBug12.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE8 = new Image("images/Sprites/Characters/LadyBug/LadyBug13.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE9 = new Image("images/Sprites/Characters/LadyBug/LadyBug14.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE10 = new Image("images/Sprites/Characters/LadyBug/LadyBug15.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE11 = new Image("images/Sprites/Characters/LadyBug/LadyBug16.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE12 = new Image("images/Sprites/Characters/LadyBug/LadyBug17.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE13 = new Image("images/Sprites/Characters/LadyBug/LadyBug18.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	public final static Image LADYBUG_IMAGE14 = new Image("images/Sprites/Characters/LadyBug/LadyBug19.png",Ladybug.LADYBUG_WIDTH,Ladybug.LADYBUG_WIDTH,false,false);
+	
 	private final static int LADYBUG_WIDTH = 64;
 
 	public Ladybug(String name, int x, int y){
@@ -43,6 +57,49 @@ public class Ladybug extends Sprite{
 		}
     }
 	
+	public void deathAnimation(long currentMilli, long frozenMilli) {
+		if(1400<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=1500) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE2);
+		}
+		else if(1300<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=1400) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE3);
+		}
+		else if(1200<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=1300) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE4);
+		}
+		else if(1100<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=1200) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE5);
+		}
+		else if(1000<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=1100) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE6);
+		}
+		else if(900<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=1000) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE7);
+		}
+		else if(800<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=900) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE8);
+		}
+		else if(700<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=800) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE9);
+		}
+		else if(600<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=700) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE10);
+		}
+		else if(500<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=600) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE11);
+		}
+		else if(400<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=500) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE12);
+		}
+		else if(300<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=400) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE13);
+		}
+		else if(200<=frozenMilli-currentMilli&&frozenMilli-currentMilli<=300) {
+			this.loadImage(Ladybug.LADYBUG_IMAGE14);
+		}
+		
+	}
+	
 
 	public void move() {
 		int tempX = this.x + this.dx;
@@ -54,3 +111,4 @@ public class Ladybug extends Sprite{
 			this.y = tempY;    
 	}
 }
+

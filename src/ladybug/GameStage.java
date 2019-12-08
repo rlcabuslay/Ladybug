4ef63fbbe34e25bd97b2f40155a2a8e3bf98f850
Hyperhawk69 <58247329+Hyperhawk69@users.noreply.github.com>
2019-12-08 22:28:39 +0800

    updated initial top image

--- src/ladybug/GameStage.java
+++ src/ladybug/GameStage.java
@@ -49,7 +49,7 @@ public class GameStage {
 	public static boolean IS_GAME_DONE = false;
 	
 	public final static Image bg = new Image("images/LadyBugMaze.png", GameStage.MAP_WIDTH, GameStage.MAP_HEIGHT, false, false);
-	public final static Image top = new Image("images/Sprites/Arcade - Lady Bug - Maze (5).png", GameStage.MAP_WIDTH, GameStage.CELL_SIZE, false, false);
+	public final static Image top = new Image("images/Sprites/Arcade - Lady Bug - Maze (4).png", GameStage.MAP_WIDTH, GameStage.CELL_SIZE, false, false);
 	public final static Image gameover = new Image("images/Sprites/GameOver.png", GameStage.MAP_WIDTH, GameStage.MAP_HEIGHT, false, false);
 	
 	public GameStage() {
